5ca9e85ac952b0e8246f43283c463097228e2e5f
LiamShiu <indoor.raptor787@gmail.com>
2025-04-30 11:06:54 -0500

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,9 @@
 #this is a python file used for documenting our knowledge of python
 
-#finds character value of given variable
+#finds character value of given variable and prints it under a new variable
 spencer = 'loser'
-print(spencer.find('l'))
+index = print(spencer.find('l'))
+print(index)
 
 #finds what is in slot six. you can use negatives to find charachters
 t = 'Hello World'
