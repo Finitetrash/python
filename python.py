70568213395d1a84c3b6e7b02f7cd5df068613c3
LiamShiu <indoor.raptor787@gmail.com>
2025-04-30 10:59:57 -0500

    stuff

--- python.py
+++ python.py
@@ -1,13 +1,14 @@
 #this is a python file used for documenting our knowledge of python
 
 #finds character value of given variable
-spencer = 'lame'
+spencer = 'loser'
 print(spencer.find('l'))
+
 #finds what is in slot six. you can use negatives to find charachters
 t = 'Hello World'
 print(t[6])
 
-
+print('why are you mean')
 
 
 
