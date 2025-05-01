86b055d2b6cfa05d9b8ca1362034fb281d253b2f
Finitetrash <spencersullivan318@gmail.com>
2025-05-01 13:32:35 -0500

    stuff

--- python.py
+++ python.py
@@ -25,18 +25,15 @@ print(type(text))
 ys = 'YOU SUCK'
 print(ys.lower())
 
-#using lower w/ find
+#finds the slot h(text[0].lower) within the alphabet var
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 index = alphabet.find(text[0].lower())
 print(index)
+#output = 7
 
 #a use of variables, .find, .lower, and adding a number var to a string var to find the position of
 shift = 3
 
-#finds the slot h(text[0].lower) within the alphabet var
-index = alphabet.find(text[0].lower())
-print(index)
-#output = 7
 
 shifted2 = alphabet[index]
 print(shifted2)
@@ -46,6 +43,19 @@ print(shifted2)
 shifted = alphabet[index + shift]
 print(shifted)
 #output = k
+
+#this is a for loop.
+#it take 'chores' seperates the strings into 'chore' and then out puts it the amount of seperate stings that are in 'chores'
+
+chores = ["wash dishes", "sweep floor", "take out trash"]
+
+for chore in chores:
+    print("I'm going to", chore)
+
+#output
+#I'm going to wash dishes
+#I'm going to sweep floor
+#I'm going to take out trash
 
 
 
