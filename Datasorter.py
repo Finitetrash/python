e779f50c69cc255b553dca1b9963335d88eb8e8f
Finitetrash <spencersullivan318@gmail.com>
2025-07-11 09:40:55 -0500

    Update Datasorter.py

--- Datasorter.py
+++ Datasorter.py
@@ -1,12 +1,21 @@
-data = [1,0,7,4,-9,4,2]
+data = [1, 0, 7, 4, -9, 4, 2]
 
-for DataSet in data:
-  if DataSet == 0:
-    print("no income")
-  elif DataSet > 0:
-    print(f"income: {DataSet}")
-  else:
-    print(f"loss: {DataSet}")
-  
-  
+def sort_data(data):
+    data.sort()
+    return data
+
+sorted_data = sort_data(data)
+
+def run_sorted(data):
+    for DataSet in data:
+        if DataSet == 0:
+            print("no income or loss")
+        elif DataSet > 0:
+            print(f"income: {DataSet}")
+        else:
+            print(f"loss: {DataSet}")
+
+run_sorted(sorted_data)
+
 print(f"total income: {sum(data)}")
+ 
