725546342935c24b25d606d5297752416881e834
Finitetrash <spencersullivan318@gmail.com>
2025-07-11 08:53:36 -0500

    Update Datasorter.py

--- Datasorter.py
+++ Datasorter.py
@@ -1,6 +1,12 @@
-data = [1,0,7,4,9,4,2]
+data = [1,0,7,4,-9,4,2]
 
 for DataSet in data:
-  if data >= 0:
-    print"no income"
-
+  if DataSet == 0:
+    print("no income")
+  elif DataSet > 0:
+    print(f"income: {DataSet}")
+  else:
+    print(f"loss: {DataSet}")
+  
+  
+print(f"total income: {sum(data)}")
